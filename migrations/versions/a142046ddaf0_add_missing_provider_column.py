74831b9fc02930bfa65f63626c3d84cbf7760565
Shruti-lab <124026034+Shruti-lab@users.noreply.github.com>
2025-11-19 13:36:47 +0530

    pushed latest flask db migrations

---
+++ migrations/versions/a142046ddaf0_add_missing_provider_column.py
@@ -0,0 +1,24 @@
+"""Add missing provider column
+
+Revision ID: a142046ddaf0
+Revises: 
+Create Date: 2025-11-19 13:34:32.719349
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'a142046ddaf0'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
